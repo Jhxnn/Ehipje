efe08e0bfe7737d7a0cab27335036df8ddb2523b
Hilixx <jhonalizan@gmail.com>
2024-11-23 12:53:04 -0300

    userService

--- src/main/java/com/Ehipje/services/UserService.java
+++ src/main/java/com/Ehipje/services/UserService.java
@@ -1,5 +1,39 @@
 package com.Ehipje.services;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Ehipje.dtos.UserDto;
+import com.Ehipje.models.User;
+import com.Ehipje.repositories.UserRepository;
+
+@Service
 public class UserService {
 
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(UUID id) {
+		return userRepository.findById(id).orElseThrow(() -> new RuntimeException("Cannot be found"));
+	}
+	
+	public User createUser(UserDto userDto) {
+		var user = new User();
+		BeanUtils.copyProperties(userDto, user);
+		return userRepository.save(user);
+	}
+	
+	public User updateUser(UserDto userDto, UUID id) {
+		
+	}
 }
